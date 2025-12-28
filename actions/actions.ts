15530388591ec975f77f2febe81e16fbbe820fc4
aditehh <aditya.aryal888@gmail.com>
2025-12-28 21:41:03 +0545

    this

---
+++ actions/actions.ts
@@ -0,0 +1,35 @@
+"use server"
+import prisma from "@/lib/db"
+import { revalidatePath } from "next/cache"
+
+
+export async function createPost(formData: FormData) {
+    await prisma.post.create({
+        data: {
+            title: formData.get("title") as string,
+            slug: (formData.get("title") as string)
+                .replace(/\s+/g, "-")
+                .toLocaleLowerCase(),
+            content: formData.get("content") as string
+        },
+    })
+
+    revalidatePath("/posts");
+}
+
+export async function editPost(formdata: FormData, slug: string) {
+    await prisma.post.update({
+        where: { slug },
+        data: {
+            title: formdata.get("title") as string,
+            slug: (formdata.get("title") as string)
+                .replace(/\s+/g, "-")
+                .toLowerCase(),
+            content: formdata.get("content") as string
+        }
+    })
+}
+
+export async function deletePost(slug: string) {
+    await prisma.post.delete({ where: { slug } });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 import prisma from "@/lib/db";
 import Link from "next/link";
+import { createPost } from "@/actions/actions";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany({
+    // where: {
+    //   title: {
+    //     startsWith: "this"
+    //   },
+    // },
+    // orderBy: {
+    //   createdAt: "desc"
+    // },
+    // select: {
+    //   id: true,
+    //   title: true,
+    //   slug: true
+    // },
+  });
 
   return (
-    <main style={{ padding: 20 }}>
+    <main
+      className="flex flex-col justify-items-center items-center" style={{ padding: 20 }}>
       <h1>Posts in the database ({posts.length})</h1>
 
       <ul className="pt-20">
@@ -20,6 +36,28 @@ export default async function Home() {
         ))}
 
       </ul>
+
+      <form
+        action={createPost}
+        className="flex flex-col gap-y-2 w-75 pt-30">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="px-2 py-1 rounded-sm"
+        />
+        <textarea
+          name="content"
+          rows={5}
+          placeholder="Content"
+          className="px-2 py-1 rounded-sm"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 py-2 text-white rounded-sm">
+          Create Post
+        </button>
+      </form>
 
     </main >
 
