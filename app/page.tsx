6be1d1b7d9b20d3508091bf0ccefe2f7925f6a2f
aditehh <aditya.aryal888@gmail.com>
2025-12-28 17:53:05 +0545

    secondary commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,26 @@ export default async function Home() {
   const posts = await prisma.post.findMany();
 
   return (
-    <main style={{ padding: 20 }}>
-      <h1>Posts in the database</h1>
+    // <main style={{ padding: 20 }}>
+    //   <h1>Posts in the database</h1>
 
-      <ul>
-        {posts.map((p) => (
-          <li key={p.id}>
-            <strong>{p.title}</strong>
-            <p>{p.content}</p>
-          </li>
-        ))}
+    //   <ul>
+    //     {posts.map((p) => (
+    //       <li key={p.id}>
+    //         <strong>{p.title}</strong>
+    //         <p>{p.content}</p>
+    //       </li>
+    //     ))}
 
-      </ul>
+    //   </ul>
 
-    </main>
+    // </main>
+
+<main>
+  click me to 
+</main>
+
+
   );
 }
                  
